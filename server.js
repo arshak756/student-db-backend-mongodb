3ab3206d0fdae41daa63c2362a4b124d96d81f8f
arshak756 <mohammedarshak793@gmail.com>
2025-06-13 20:22:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const express = require('express');
-const mongoose = require('mongoose');
+
 const cors = require('cors');
 
 const app = express();
